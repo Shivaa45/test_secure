8f5145e697c325832e057b22995d4b710a4cbd7c
Shivaa45 <89933976+Shivaa45@users.noreply.github.com>
2023-07-06 21:56:28 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Read the access key value from the environment variable
+
+access_key_value="$(az keyvault secret show --vault-name tf-state1 --name tf-state-AK --query value -o tsv)"
+
+# Define the file path
+file_path="./backend.tf"
+
+# Update the access_key variable in the file
+# sed -i -e "/access_key =/ s/= .*/= '$access_key_value'/" ./main.tf
+sed -i -e "/access_key =/ s/= .*/= \""$(echo $access_key_value)"\"/" ./main.tf
