5ed79382f713671313a9eb4107b0fb3b657dc8c5
Shivaa45 <89933976+Shivaa45@users.noreply.github.com>
2023-07-06 17:12:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
         storage_account_name = "shivammaurya286"
         container_name       = "tfstate"
         key                  = "terraform.tfstate"
-        access_key = "TXvlNQ3eJwuvhASL4Vs8Vf0+bIXl2or8MRUrhWZ0Ug6rXaag1TavHqGFn9Z8923EDKRrKZJeyes8+ASt7Kj1Cg=="
+        access_key = ""
     }
 
 }
