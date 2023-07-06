a9608250e33149c30354059012feefb1c9ed1619
Shivaa45 <89933976+Shivaa45@users.noreply.github.com>
2023-07-06 19:50:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
     backend "azurerm" {
         resource_group_name  = "tfstate"
         storage_account_name = "shivammaurya286"
-        container_name       = "tfstate1"
+        container_name       = "tfstate"
         key                  = "terraform.tfstate"
         access_key           = "${access_key_output}"
     }
